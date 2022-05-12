55933b8b4a5f5b47d6c567e7a4060a93cd68284a
Kibet Rono <ckibetrono@gmail.com>
2022-05-12 22:23:06 +0300

    added authentication, simple navigation and database functions

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,5 +1,5 @@
 defmodule Auction do
-  alias Auction.{Item}
+  alias Auction.{Bid, Repo, Item, User, Password}
 
   @repo Auction.Repo
 
@@ -16,8 +16,8 @@ defmodule Auction do
   end
 
   def insert_item(attrs) do
-    Item
-    |> struct(attrs)
+    %Item{}
+    |> Item.changeset(attrs)
     |> @repo.insert()
   end
 
@@ -27,5 +27,37 @@ defmodule Auction do
     item
     |> Item.changeset(attrs)
     |> @repo.update()
+  end
+
+  def new_item, do: Item.changeset(%Item{})
+
+  def edit_item(id) do
+    get_item(id)
+    |> Item.changeset()
+  end
+
+  def get_user(id), do: @repo.get!(User, id)
+
+  def new_user, do: User.changeset_with_password(%User{})
+
+  def insert_user(params) do
+    %User{}
+    |> User.changeset_with_password(params)
+    |> @repo.insert
+  end
+
+  def get_user_by_username_and_password(username, password) do
+    with user when not is_nil(user) <- @repo.get_by(User, %{username: username}),
+      true <- Password.verify_with_hash(password, user.hashed_password) do
+        user
+      else
+        _ -> Password.dummy_verify
+      end
+  end
+
+  def insert_bid(params) do
+    %Bid{}
+    |> Bid.changeset(params)
+    |> @repo.insert()
   end
 end
